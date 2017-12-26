b62881aebbba2e2b3a82a2dcaa5113fd575cee10
f0733966 <andersonmarques@bb.com.br>
2017-12-26 18:49:17 +0000

    Inclusão do ignorefile e dos fontes iniciais do projeto

---
+++ src/main/java/lab/bb/oat/hazelcast/laboratorio_cache_hazelcast/ServerNode.java
@@ -0,0 +1,22 @@
+package lab.bb.oat.hazelcast.laboratorio_cache_hazelcast;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+public class ServerNode 
+{
+    public static void main( String[] args )
+    {
+        new ServerNode().iniciarServer();
+    }
+    
+    public void iniciarServer(){
+    	HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance();
+    	
+    	IMap<String, String> funcisNotaDez = hzInstance.getMap("br.com.bb.oat.caches.funcionarios-nota-dez");
+    	
+    	funcisNotaDez.put("f0733966", "anderson");
+    	
+    }
+}
---
+++ src/main/java/lab/bb/oat/hazelcast/laboratorio_cache_hazelcast/ServerNode2.java
@@ -0,0 +1,20 @@
+package lab.bb.oat.hazelcast.laboratorio_cache_hazelcast;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+public class ServerNode2 
+{
+    public static void main( String[] args )
+    {
+        new ServerNode2().iniciarServer();
+    }
+    
+    public void iniciarServer(){
+    	HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance();
+    	
+    	
+    	System.out.println(hzInstance.getMap("br.com.bb.oat.caches.funcionarios-nota-dez").get("f0733966"));
+    	
+    }
+}
